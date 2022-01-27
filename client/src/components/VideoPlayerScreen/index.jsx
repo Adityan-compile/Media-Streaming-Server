7cfb28ec43bacc4f1f8d7deb6d168c8b855333d8
Adityan-compile <67089770+Adityan-compile@users.noreply.github.com>
2022-01-27 20:36:24 +0530

    Move from video-react to vidmond/replay for video playback

--- client/src/components/VideoPlayerScreen/index.jsx
+++ client/src/components/VideoPlayerScreen/index.jsx
@@ -3,41 +3,47 @@ import "vimond-replay/index.css";
 
 import { useLocation, useNavigate } from "react-router-dom";
 
-import { Button } from "primereact/button";
-import React from "react";
 import { Replay } from "vimond-replay";
+import { useState } from "react";
 
 function VideoPlayerScreen() {
+  
   const navigate = useNavigate();
   const { state } = useLocation();
   const data = state.data;
-
+   
+  const [initialLoad,setInitialLoad] = useState(true);
+  
+  // Implement Stream Ended State Navigation
+  const handleStreamStateChange = (state)=>{
+    if(initialLoad){
+      setInitialLoad(false);
+      return;
+    }
+    if(state){
+      if('playState' in state){
+        if(state.playState === 'inactive' && !initialLoad){
+          console.log("Ended")
+        }
+      }
+    }
+  }
   return (
     <div className="player-container">
-      {/* <div className="overlay-btn">
-        <Button
-          icon="pi pi-arrow-left"
-          className="p-button-rounded p-button-outlined p-button-secondary btn"
-          onClick={() => {
-            navigate("/shows/view", { state: { data } });
-          }}
-        />
-      </div> */}
-      <div className="player">
-        <Replay
-          onEnd={() => {
-            setTimeout(
-              () => navigate("/shows/view", { state: { data } }),
-              1000
-            );
-          }}
-          initialPlaybackProps={{
-            isPaused: true
-          }}
-          source={`/api/media/movies/stream?file=${data.file}`}
-          className="video-player"
-        />
-      </div>
+      <Replay
+        onEnd={() => {
+          console.log("Video Ended")
+          setTimeout(() => navigate("/shows/view", { state: { data } }), 1000);
+        }}
+        onExit={() => {
+          navigate("/shows/view", { state: { data } });
+        }}
+        initialPlaybackProps={{
+          volume: 0.6
+        }}
+        source={`/api/media/movies/stream?file=${data.file}`}
+        onStreamStateChange={handleStreamStateChange}
+      />
     </div>
   );
 }
