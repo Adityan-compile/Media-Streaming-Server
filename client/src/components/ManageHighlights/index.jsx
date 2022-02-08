0cca8b7a82939316324f9fd1d7bc9a2d867bbc49
Adityan-compile <67089770+Adityan-compile@users.noreply.github.com>
2022-02-08 19:49:40 +0530

    Add Search Method for Highlights

--- client/src/components/ManageHighlights/index.jsx
+++ client/src/components/ManageHighlights/index.jsx
@@ -1,7 +1,14 @@
+import {Button} from "primereact/button";
 import React from 'react';
 
 function ManageHighlights() {
-  return (<div></div>);
+  return (
+  <div style={{
+    margin: "20px"
+  }}>
+    <Button label="New Highlight" icon="pi pi-plus" />
+  </div>
+  );
 }
 
 export default ManageHighlights;
--- servers/master-server/controllers/mediaController/index.js
+++ servers/master-server/controllers/mediaController/index.js
@@ -1,5 +1,6 @@
-const { movies, highlights } = require("../../models");
+const { movies, highlights, shows } = require("../../models");
 const streamer = require("express-stream-video");
+const res = require("express/lib/response");
 
 exports.getMovies = async (req, res) => {
   try {
@@ -25,10 +26,10 @@ exports.streamMovie = (req, res) => {
 
   const filePath = `/var/app/uploads/${filename}`;
 
-  streamer.stream(req,res,filePath);
+  streamer.stream(req, res, filePath);
 };
 
-exports.getHighlights = async(req,res) =>{
+exports.getHighlights = async (req, res) => {
   try {
     const results = await movies.findAll({});
     res.status(200).json({
@@ -40,12 +41,12 @@ exports.getHighlights = async(req,res) =>{
   }
 };
 
-exports.createHighlight = async(req,res)=>{
+exports.createHighlight = async (req, res) => {
   const body = req.body;
-  if(!body ||Object.keys(body).length === 0) {
+  if (!body || Object.keys(body).length === 0) {
     return res.status(400).json({
       status: 400,
-      message: "Bad Request"
+      message: "Bad Request",
     });
   }
 
@@ -57,15 +58,65 @@ exports.createHighlight = async(req,res)=>{
     });
 
     res.status(200).json({
-      status:  200,
+      status: 200,
       message: "Highlight Created",
-      highlight: newHighlight.toJSON()
+      highlight: newHighlight.toJSON(),
     });
   } catch (e) {
     res.status(500).json({
       status: 500,
-      message:  "Error Creating Highlight"
+      message: "Error Creating Highlight",
+    });
+  }
+};
+
+exports.search = async (req, res) => {
+  const query = req.query.q;
+
+  if (!query) {
+    return res.status(400).json({
+      status: 400,
+      message: "Bad Request",
     });
   }
 
+  try {
+    const movie_results = await movies.findAll({
+      where: {
+        $or: [
+          {
+            name: {
+              $iLike: query,
+            },
+          },
+        ],
+      },
+    });
+
+    const show_results = awaitshows.findAll({
+      where: {
+        $or: [
+          {
+            name: {
+              $iLike: query,
+            },
+          },
+        ],
+      },
+    });
+
+    return res.status(200).json({
+      status: 200,
+      message: "Search Successful",
+      results: {
+        movies: movie_results,
+        shows: show_results,
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "Database Query Error",
+    });
+  }
 };
