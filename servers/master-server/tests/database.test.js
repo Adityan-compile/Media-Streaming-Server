09b4842e34690e4fcc3985029f7194d6786424f8
Adityan-compile <67089770+Adityan-compile@users.noreply.github.com>
2022-03-16 11:38:24 +0530

    Add Tests for Server and Shows Model

--- servers/master-server/tests/database.test.js
+++ servers/master-server/tests/database.test.js
@@ -1,4 +1,4 @@
-const {movie} = require("../models");
+const {movies, shows, server} = require("../models");
 const migrate = require("../config/database/migrate");
 
 describe("Database Tests", () => {
@@ -7,8 +7,8 @@ describe("Database Tests", () => {
   });
 
   test("Can Create Movie", async () => {
-    expect.assertions(5);
-    const newMovie = await movie.create({
+    expect.assertions(6);
+    const newMovie = await movies.create({
       name: "test",
       tagline: "test",
       description: "test",
@@ -19,9 +19,37 @@ describe("Database Tests", () => {
     expect(newMovie.tagline).toBe("test");
     expect(newMovie.description).toBe("test");
     expect(newMovie.lang).toBe("en");
+    expect(newMovie.adult).toBe(false);
+  });
+
+  test("Can Create Show", async () => {
+    expect.assertions(5);
+    const newServer = await server.create({
+      name: "test",
+      description: "test",
+      lang: "en",
+    });
+    expect(newShow.id.toString().length).toBe(20);
+    expect(newShow.name).toBe("test");
+    expect(newShow.description).toBe("test");
+    expect(newShow.lang).toBe("en");
+    expect(newShow.adult).toBe(false);
 
   });
 
-  test.todo("Can Create Show");
-  test.todo("Can Create Server");
+  test("Can Create Server", async ()=>{
+    expect.assertions(5);
+    const newShow = await shows.create({
+      name: "test",
+      tmdbKey: "test key",
+      videoQuality: "1920x?",
+      audioQuality: "256k"
+    });
+    expect(newShow.id.toString().length).toBe(20);
+    expect(newShow.name).toBe("test");
+    expect(newShow.tmdbKey).toBe("test key");
+    expect(newShow.videoQuality).toBe("1920x?");
+    expect(newShow.audioQuality).toBe("256k");
+    expect(newShow.transcoder).toBe(true);
+  });
 });
